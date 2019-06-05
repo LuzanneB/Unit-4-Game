595fda309d4ed8b3eec6059fa764e015f83c82d9
Luzanne Burt <luzannea@gmail.com>
2019-06-04 20:14:22 -0400

    psuedo code for JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Create a variable called randomNumber to equal a random number between 1-100 and
+    // Show value in #randomNumber
+// Create a variable called crystal1, crystal2, crystal3,crystal4 all to random # between 1-100.
+// Create a variable called totalScore equal to 0.
+// Create a variable called wins equal to 0
+// Create a variable called losses equal to 0
+// Create a function to reset randomNumber, crystal1-4, totalScore
+// Create click event, so that when crystal images are clicked the current value of crystal adds to the totalScore value 
+    // if the totalScore is less than randomNumber, update #totalScore with totalScore
+    // if the totalScore is more than randomNumber,
+        // +1 to losses variable and update #losses
+        // run reset function created above
+    // if the totalScore is equal to randomNumber
+        // +1 to wins variable and update #wins
+        // run reset function created above
